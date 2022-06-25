f057b9870221b85b03d38d777a66e608f807d456
bossthanawat <boss41680@gmail.com>
2022-06-25 18:02:10 +0700

    remove urlPrefix mock

--- src/_mock/mirage.ts
+++ src/_mock/mirage.ts
@@ -2,7 +2,6 @@ import { createServer } from 'miragejs';
 export function makeServer() {
   return createServer({
     routes() {
-      this.urlPrefix = process.env.REACT_APP_API_URL_BACKEND || "https://test-react-mock.com";
       this.passthrough();
       this.timing = 1000
       this.get(
--- src/_services/index.tsx
+++ src/_services/index.tsx
@@ -1,6 +1,5 @@
 import axios, { AxiosPromise } from "axios";
-const urlBackend = process.env.REACT_APP_API_URL_BACKEND || "https://test-react-mock.com";
 
 export const getUserById = ({ id }: any): AxiosPromise<any> => {
-  return axios.get(`${urlBackend}/user/${id}`)
+  return axios.get(`/user/${id}`)
 };
